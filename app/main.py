c765cb09c91fc81d6296918b27089c9a469de433
Jan Haans <jan.haans@gmail.com>
2021-01-12 21:26:10 +0100

    Path Parameter

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {'message': 'Hello World!'}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int, name: str):
+    return {"item_id": item_id, "name": name}
