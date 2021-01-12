c8fb34e1da9c106d3b8e9e3c6f1311ca8c1307d6
Jan Haans <jan.haans@gmail.com>
2021-01-12 23:55:43 +0100

    Request Body

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class ItemModel(BaseModel):
+    name: str
+    price: float
+    quantity: int
 
 @app.get("/")
 async def root():
     return {'message': 'Hello World!'}
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: int, name: str):
-    return {"item_id": item_id, "name": name}
+@app.get("/item/{item_id}")
+async def get_item(item_id: int, item: ItemModel):
+    item_dict = item.dict()
+    response = { "item_id": item_id }
+    response.update(item_dict)
+    return response
