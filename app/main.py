e2523788b8658cf21a4e9de11abaa43ca49ed5a5
Jan Haans <jan.haans@gmail.com>
2021-01-13 22:03:54 +0100

    Added GET and POST operations for item reource

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class ItemModel(BaseModel):
-    name: str
+items_by_name = {}
+
+class Item(BaseModel):
     price: float
     quantity: int
 
@@ -12,9 +13,20 @@ class ItemModel(BaseModel):
 async def root():
     return {'message': 'Hello World!'}
 
-@app.get("/item/{item_id}")
-async def get_item(item_id: int, item: ItemModel):
-    item_dict = item.dict()
-    response = { "item_id": item_id }
-    response.update(item_dict)
-    return response
+@app.get("/item/{name}")
+async def get_item( name: str = Path(..., title="Item name") ):
+    item = items_by_name.get(name)
+    if item is None:
+        raise HTTPException(status_code=404, detail=f'Item {name} is not in the database')
+    return { **items_by_name[name] }
+    
+
+@app.post("/item/{name}")
+async def post_item( *,
+                 name: str = Path(..., title="Item name"), 
+                 item: Item ):
+    found_item = items_by_name.get(name)
+    if found_item:
+        raise HTTPException(status_code=400, detail=f'Item {name} is in the database')
+    items_by_name[name] = { 'name': name, **item.dict() }
+    return { 'message': f'Item {name} is successfully saved' }
