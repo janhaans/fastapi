0fa34fd5e304b26513aacd4251787e33001162a7
Jan Haans <jan.haans@gmail.com>
2021-01-15 21:37:57 +0100

    Intoduced Dependency and Output model

--- app/main.py
+++ app/main.py
@@ -1,32 +1,40 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Depends
 from pydantic import BaseModel
 
 app = FastAPI()
 
 items_by_name = {}
 
-class Item(BaseModel):
+class ItemIn(BaseModel):
     price: float
     quantity: int
 
-@app.get("/")
-async def root():
-    return {'message': 'Hello World!'}
+class ItemInDb(ItemIn):
+    name: str
 
-@app.get("/item/{name}")
-async def get_item( name: str = Path(..., title="Item name") ):
+class ItemOut(ItemInDb):
+    pass
+
+async def verify_item_not_in_db(name: str):
     item = items_by_name.get(name)
     if item is None:
         raise HTTPException(status_code=404, detail=f'Item {name} is not in the database')
+
+async def verify_item_in_db(name: str):
+    item = items_by_name.get(name)
+    if item is not None:
+        raise HTTPException(status_code=404, detail=f'Item {name} is in the database')
+
+def save_item_in_db(name: str, item: ItemIn):
+    item_in_db = ItemInDb(name=name, **item.dict()) 
+    items_by_name[name] = { **item_in_db.dict() }
+    return item_in_db
+
+@app.get("/item/{name}", response_model=ItemOut, dependencies=[Depends(verify_item_not_in_db)])
+async def get_item(name: str):
     return { **items_by_name[name] }
     
 
-@app.post("/item/{name}")
-async def post_item( *,
-                 name: str = Path(..., title="Item name"), 
-                 item: Item ):
-    found_item = items_by_name.get(name)
-    if found_item:
-        raise HTTPException(status_code=400, detail=f'Item {name} is in the database')
-    items_by_name[name] = { 'name': name, **item.dict() }
-    return { 'message': f'Item {name} is successfully saved' }
+@app.post("/item/{name}", response_model=ItemOut, dependencies=[Depends(verify_item_in_db)])
+async def post_item(name: str, item: ItemIn):
+    return save_item_in_db(name, item)
